44adbbafe515ef8e09811a0dde115731b04572a1
ringggr <ringggr@icloud.com>
2021-07-18 16:38:38 +0800

    create project

---
+++ src/main/java/com/ringggr/demoitnan/DemoitnanApplication.java
@@ -0,0 +1,13 @@
+package com.ringggr.demoitnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoitnanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoitnanApplication.class, args);
+    }
+
+}
